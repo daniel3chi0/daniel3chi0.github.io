cb572cecd41a7f4853410e4f181ca4d80cd7447d
DaNiel3Chio <843224210@qq.com>
2020-02-18 03:29:32 +0800

    Site updated: 2020-02-18 03:29:31

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -499,12 +499,12 @@ $(document).ready(function () {
       setCookie('bgImgSetting', url, 30)
     })
   }
-  changeBG('#sakura-bg', 'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/themebg/sakura.png')
-  changeBG('#gribs-bg', 'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/themebg/plaid.jpg')
-  changeBG('#pixiv-bg', 'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/themebg/star.png')
-  changeBG('#KAdots-bg', 'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/themebg/point.png')
-  changeBG('#totem-bg', 'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/themebg/little-monster.png')
-  changeBGnoTrans('#bing-bg', 'https://api.shino.cc/bing/')
+  changeBG('#sakura-bg', 'https://cdn.jsdelivr.net/gh/DaNiel3Chio/cdn@1.5/img/themebg/sakura.png')
+  changeBG('#gribs-bg', 'https://cdn.jsdelivr.net/gh/DaNiel3Chio/cdn@1.5/img/themebg/plaid.jpg')
+  changeBG('#pixiv-bg', 'https://cdn.jsdelivr.net/gh/DaNiel3Chio/cdn@1.5/img/themebg/star.png')
+  changeBG('#KAdots-bg', 'https://cdn.jsdelivr.net/gh/DaNiel3Chio/cdn@1.6/img/themebg/dog.png')
+  changeBG('#totem-bg', 'https://cdn.jsdelivr.net/gh/DaNiel3Chio/cdn@1.5/img/themebg/little-monster.png')
+  changeBGnoTrans('#bing-bg', 'https://cdn.jsdelivr.net/gh/DaNiel3Chio/cdn@1.5/img/themebg/denim.jpg')
   $('.skin-menu #white-bg').click(function () {
     mashiro_global.variables.skinSecter = false
     mashiro_global.variables.isNight = false
@@ -521,7 +521,7 @@ $(document).ready(function () {
   $('.skin-menu #dark-bg').click(function () {
     mashiro_global.variables.skinSecter = true
     mashiro_global.variables.isNight = true
-    $('body').css('background-image', 'url(https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/other/starry_sky.png)')
+    $('body').css('background-image', 'url(https://cdn.jsdelivr.net/gh/DaNiel3Chio/cdn@1.5/img/themebg/billboard.png)')
     $('.blank').css('background-color', 'rgba(255,255,255,.8)')
     $('#night-mode-cover').css('visibility', 'visible')
     $('.pattern-center').removeClass('pattern-center').addClass('pattern-center-sakura')
